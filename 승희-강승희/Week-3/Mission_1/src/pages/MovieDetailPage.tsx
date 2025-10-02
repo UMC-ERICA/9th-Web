4b0b648560d3f6702b632f761b53e7a2a2022e29
xancxoxin <judy010321@naver.com>
2025-10-02 11:13:27 +0900

    3주차 미션2_승희

---
+++ 승희-강승희/Week-3/Mission_1/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+
+const MovieDetailPage = () : Element => {
+    const params = useParams();
+    console.log(params);
+
+    return <div>MovieDetailPage</div>
+}
+
+export default MovieDetailPage;
