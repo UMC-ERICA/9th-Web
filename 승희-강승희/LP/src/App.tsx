84c81512270080a0a8b5bd164bc477d845ba1898
xancxoxin <judy010321@naver.com>
2025-11-02 16:12:52 +0900

    4주차 미션2_승희

---
+++ 승희-강승희/LP/src/App.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import NotFoundPage from './pages/NotFoundPage'
+import HomeLayout from './layouts/HomeLayout'
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3. 회원가입 페이지
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index:true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage />},
+    ]
+  }
+])
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
