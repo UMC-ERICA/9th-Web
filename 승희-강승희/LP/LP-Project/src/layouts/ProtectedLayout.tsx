be9125aa3edc98b04cf18a5a5ccd9fb847f4da17
xancxoxin <judy010321@naver.com>
2025-11-17 16:38:15 +0900

    6주차 미션_승희

--- 승희-강승희/LP/LP-Project/src/layouts/ProtectedLayout.tsx
+++ 승희-강승희/LP/LP-Project/src/layouts/ProtectedLayout.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Navbar from "../components/Navbar";
 
 const ProtectedLayout = () => {
     const {accessToken} = useAuth();
@@ -8,7 +9,15 @@ const ProtectedLayout = () => {
     if(!accessToken){
         return <Navigate to = {'/login'} state={{location}} replace />;
     }
-    return <Outlet />;
+    return (
+     <>
+         <Navbar />
+         <main className="pt-20"> {/* Navbar에 겹치지 않도록 pt-20 추가 권장 */}
+                <Outlet />
+            </main>
+     </>
+    );
+    
 }
 
 export default ProtectedLayout;
